77bbd6bb1e964c7aeb946806504a0c551f8db61c
Evandro-git20 <evandro.ribeiro@unigranrio.br>
2021-07-18 19:58:07 -0300

    primeira edição.

--- parte-1/src/br/com/parte1/basico/troco/Principal.java
+++ parte-1/src/br/com/parte1/basico/troco/Principal.java
@@ -1,11 +1,28 @@
 package br.com.parte1.basico.troco;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// Faça um programa que receba um valor que é o valor pago, um segundo valor que é o preço do produto
 		// e retorne o troco a ser dado.
-
+		
+		double pago, preco, troco;
+		pago = preco = troco = 0;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Valor pago R$ ");
+		pago = in.nextDouble();
+		
+		System.out.print("Preço R$ ");
+		preco = in.nextDouble();
+		
+		troco = pago - preco;
+		
+		System.out.print("Troco R$ " + troco);
+		
 	}
 
 }
