8ce780fae9a7fe2daaa4f9db7f869fff713d9952
Evandro-git20 <evandro.ribeiro@unigranrio.br>
2021-07-18 11:29:38 -0300

    Configurando projeto básico.

---
+++ parte-1/src/br/com/parte1/basico/troco/Principal.java
@@ -0,0 +1,11 @@
+package br.com.parte1.basico.troco;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Faça um programa que receba um valor que é o valor pago, um segundo valor que é o preço do produto
+		// e retorne o troco a ser dado.
+
+	}
+
+}
