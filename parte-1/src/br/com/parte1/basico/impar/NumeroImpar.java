b93dd12df8a89799d5b3eb4b03673ab3221c3f31
Evandro-git20 <evandro.ribeiro@unigranrio.br>
2021-07-20 19:40:03 -0300

    Segunda edição.

--- parte-1/src/br/com/parte1/basico/impar/NumeroImpar.java
+++ parte-1/src/br/com/parte1/basico/impar/NumeroImpar.java
@@ -6,18 +6,20 @@ public class NumeroImpar {
 
 	public static void main(String[] args) {
 		// Faça um programa que verifique se um número é impar.
-		int num, resultado;
+		int num;
 		Scanner in = new Scanner(System.in);
 		
 		System.out.print("Digite um número: ");
 		num =in.nextInt();
 		
+		//if(num % 2 == 0) {
+		//	System.out.println("O número digitado não ímpar.");
+		//}else {
+		//	System.out.println("O número digitado é impar.");
+		//}
 		
-		if(num % 2 == 0) {
-			System.out.println("O número digitado não ímpar.");
-		}else {
-			System.out.println("O número digitado é impar.");
-		}
+		String j = (num % 2 == 0) ? "O número digitado é Par." : "O número digitado é Ímpar.";
+		System.out.println(j);
 
 	}
 
